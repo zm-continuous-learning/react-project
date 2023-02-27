3a5931606be01948bd33c402c12e7a0746e6c16f
zhangmiaoDev <924825856@qq.com>
2023-02-27 21:05:50 +0800

    [XIDP] feat: configure global scss

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  //配置Scss
+  css: {
+    preprocessorOptions: {
+      scss: {
+        //地址
+        additionalData: `@import "./src/styles/sassConfig.scss";`
+      }
+    }
+  }
 })
