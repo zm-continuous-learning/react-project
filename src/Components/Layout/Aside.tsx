07f764df48dc730178b735212774d2cb3a31589e
zhangmiaoDev <924825856@qq.com>
2023-02-27 22:35:50 +0800

    [XIDP] feat: menu states

--- src/Components/Layout/Aside.tsx
+++ src/Components/Layout/Aside.tsx
@@ -1,31 +1,39 @@
-import React, { useState } from "react";
-import { ROUTER_ITEM } from "../../Router/Router";
+import React, {useState} from "react";
+import {ROUTER_ITEM} from "../../Router/Router";
 import {Menu, MenuProps} from "antd";
 import {useNavigate} from "react-router-dom";
 
 const Aside = () => {
-  const [router] = useState(ROUTER_ITEM);
-  const storageSelectKeys = sessionStorage.getItem('setSelectKeys');
-  const [selectKey,setSelectKeys]=useState(storageSelectKeys);
-  const navigate = useNavigate()
-  const handlerMenu: MenuProps['onClick'] = (e) => {
-      const keyPth = e.keyPath.reverse().join("/");
-      navigate('/'+keyPth);
+    const [router] = useState(ROUTER_ITEM);
+    const storageSelectKeys = sessionStorage.getItem('setSelectKeys');
+    const [selectKey, setSelectKeys] = useState(storageSelectKeys);
+    const defaultOpenKey = JSON.parse(sessionStorage.getItem('setOpenKey'));
+    const [openKey, setOpenKey] = useState(defaultOpenKey);
+    const navigate = useNavigate()
+    const handlerMenu: MenuProps['onClick'] = (e) => {
+        const keyPth = e.keyPath;
+        const copyKeyPath = JSON.parse(JSON.stringify(keyPth));
+        setOpenKey(copyKeyPath.slice(1));
+        sessionStorage.setItem('setOpenKey', JSON.stringify(copyKeyPath.slice(1)));
 
-      //存储选中菜单的key
-      sessionStorage.setItem('setSelectKeys',e.key);
-      setSelectKeys(e.key);
+        const router = keyPth.reverse().join("/");
+        navigate('/' + router);
 
-  }
-  return (
-    <div>
-      <Menu items={router}
-            mode="inline"
-            onClick={handlerMenu}
-            defaultOpenKeys={['layout']}
-            defaultSelectedKeys={selectKey}
-      />
-    </div>
-  );
+        //存储选中菜单的key
+        sessionStorage.setItem('setSelectKeys', e.key);
+        setSelectKeys(e.key);
+
+
+    }
+    return (
+        <div>
+            <Menu items={router}
+                  mode="inline"
+                  onClick={handlerMenu}
+                  defaultOpenKeys={openKey}
+                  defaultSelectedKeys={selectKey}
+            />
+        </div>
+    );
 };
 export default Aside;
--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,22 +1,28 @@
-import { useRoutes } from "react-router-dom";
+import {useRoutes} from "react-router-dom";
 import Home from "../Pages/Home";
 import User from "../Pages/User";
 import React from "react";
 import Layout from "../Components/Layout/Layout";
 
 export const ROUTER_ITEM: Array<Object> = [
- // { path: "/", key:"/",label: "首页", element: <Navigate to="/layout/home" /> },
-  {
-    path: "/layout",
-    key:"layout",
-    label:"控制台",
-    element: <Layout />,
-    children: [
-      { path: "home",key:"home", label:"首页", element: <Home />, index: true },
-      { path: "user",key:"user", label:"用户",element: <User /> },
-    ],
-  },
+    // { path: "/", key:"/",label: "首页", element: <Navigate to="/layout/home" /> },
+    {
+        path: "/layout",
+        key: "layout",
+        label: "控制台",
+        element: <Layout/>,
+        children: [
+            {
+                path: "home", key: "home", label: "首页", element: <Home/>,
+                children: [
+                    {path: "home1", key: "home1", label: "首页1", element: <Home/>},
+                    {path: "user1", key: "user1", label: "用户1", element: <User/>},
+                ],
+            },
+            {path: "user", key: "user", label: "用户", element: <User/>},
+        ],
+    },
 ];
 export const GetRouters = () => {
-  return useRoutes(ROUTER_ITEM);
+    return useRoutes(ROUTER_ITEM);
 };
